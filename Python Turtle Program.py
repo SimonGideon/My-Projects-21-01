b5ab9b2b33a3685e42b7607d55fb45a85adcc044
SimonGideon <simongideon918.com>
2021-05-18 23:08:31 +0300

    Turtle Rainbow

---
+++ Python Turtle Program.py
@@ -0,0 +1,16 @@
+# Python Program to draw rainbow Benzene
+import turtle
+# Importing turtle library
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+t.speed(10)
+turtle.bgcolor('black')
+for x in range(360):
+    t.shape("turtle")
+    t.pencolor(colors[x % 6])
+    t.width(x/100 +2)
+    t.forward(x)
+    # t.left(59)
+    t.right(59)
+t.hideturtle()
+turtle.done()
